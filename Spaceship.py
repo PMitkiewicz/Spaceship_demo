e7536cad3568f6ab5c3417c852ca79bb422538c1
PMitkiewicz <90802134+PMitkiewicz@users.noreply.github.com>
2021-09-15 18:33:55 -0300

    Add files via upload

---
+++ Spaceship.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 18:11:37 2021
+
+@author: phenr
+"""
+
+#########################
+# Spaceship Starter Code
+#
+# Author: Your_name_here
+#########################
+
+print("Welcome traveler!")
+
+name = input("What is your name? ")
+
+print("Hello {}!".format(name))
+
+
+companions = input("How many companions do you have?")
+
+# Add your input code here
+
+
+planet = input("Where do you want to go?")
+
+# Add your code here
+companions= str(companions)
+planet= str(planet)
+
+print("The Spaceship is taking off with {",int(companions)+1,"} people to planet {",planet,"} ")
+
+#added inputs
